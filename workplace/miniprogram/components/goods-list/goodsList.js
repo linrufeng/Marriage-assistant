fb0f2e7c18ea2538741cc6871bd228ea8b02556b
李林森 <lilinsen@jd.com>
2018-11-02 19:32:38 +0800

    组件开发

--- workplace/miniprogram/components/goods-list/goodsList.js
+++ workplace/miniprogram/components/goods-list/goodsList.js
@@ -11,7 +11,17 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    goods:[
+      { 
+        id:'001',        img:'http://img30.360buyimg.com/uba/jfs/t20131/270/2071616301/436213/6feb0bee/5b45b861Nc6cfe518.png',       
+        title:'心动99手捧花',
+        stitle:'买就送价值10元表白玫瑰',
+        price:"99.99",
+        score:"5.0",
+        stock:1000
+        
+      }
+    ]
   },
 
   /**
