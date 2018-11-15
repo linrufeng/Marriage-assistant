16df10189605c17592ee956295af9d9c6d506cdb
李林森 <lilinsen@jd.com>
2018-11-15 17:18:35 +0800

    微信小程序改动

--- workplace/miniprogram/app.js
+++ workplace/miniprogram/app.js
@@ -1,15 +1,67 @@
 //app.js
 App({
+
+  globalData: {},
   onLaunch: function () {
+   
+    //获取用户权限  
+    this.getUserbase();
+    this.getuser()
+    
+  },
+  getUserbase(){
+    let _that = this;
+    return new Promise((resolev, reject)=>{
+      wx.getSetting({
+        success: res => {
+          if (res.authSetting['scope.userInfo']) {
+            wx.getUserInfo({
+              success: function (res) {
+                _that.globalData.userInfo = res.userInfo;
+                resolev(res.userInfo);
+              }
+            })
+          }
+
+        },
+        fail: res => {
+          //接口不通
+          console.log(res, 'ok')
+        }
+      })
+    })
+   
+  },
+  //获取用户id
+  getuser(){
     
     if (!wx.cloud) {
       console.error('请使用 2.2.3 或以上的基础库以使用云能力')
     } else {
-      // wx.cloud.init({
-      //   traceUser: true,
-      // })
+      wx.cloud.init({
+        traceUser: true,
+      },res=>{
+        console.log(res)
+      })
     }
-
-    this.globalData = {}
+    // wx.cloud.callFunction({
+    //   name: 'getuserMsg',
+    //   complete: res => {
+    //     console.log(res)
+    //     this.globalData.openid = res.result.OPENID;
+    //     this.creatSer()
+    //   }
+    // })
+    // wx.cloud.init()
+  
+  },
+  //创建数据库
+  creatSer(){
+    wx.cloud.callFunction({
+      name: 'creatNewuser',
+      complete: res => {
+        console.log('云函数测试', res)
+      }
+    })
   }
 })
--- workplace/miniprogram/components/goods-list/goodsList.js
+++ workplace/miniprogram/components/goods-list/goodsList.js
@@ -4,14 +4,14 @@ Component({
    * 组件的属性列表
    */
   properties: {
-
+    list:Array
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-    goods:[
+    list:[
       { 
         id:'001',        img:'http://img30.360buyimg.com/uba/jfs/t20131/270/2071616301/436213/6feb0bee/5b45b861Nc6cfe518.png',       
         title:'心动99手捧花',
@@ -28,6 +28,14 @@ Component({
    * 组件的方法列表
    */
   methods: {
-
+    //加清单 msg good id
+    addShopCar(e){
+      this.triggerEvent('addList',e,'加清单')
+    },
+    //购买 msg good id
+    buy(e){
+      console.log('1')
+      this.triggerEvent('buy',e,'购买')
+    }
   }
 })
